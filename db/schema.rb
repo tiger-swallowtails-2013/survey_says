2ba1b084a9e8a2358fe6c213719129cad610f4f1
Apprentice <apprentice@devbootcamp.com>
2013-09-19 09:30:53 -0700

    incl schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,15 +14,15 @@ ActiveRecord::Schema.define(version: 20130918183718) do
 
   create_table "questions", force: true do |t|
     t.string   "question"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "survey_id"
   end
 
   create_table "surveys", force: true do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 20130918183718) do
     t.string   "last_name"
     t.string   "password_hash"
     t.string   "email"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
