e047a58c1a883a301ec5c681f3d10136d04378dd
Apprentice <apprentice@devbootcamp.com>
2013-09-19 15:24:10 -0700

    schema timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918183718) do
+ActiveRecord::Schema.define(version: 20130919221532) do
 
   create_table "questions", force: true do |t|
     t.string   "question"
